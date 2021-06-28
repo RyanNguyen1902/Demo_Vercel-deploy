7543e96825c01048810682167e4bdf412a9f275a
Trinh <nguyenhoangtrinh39@gmail.com>
2021-06-28 09:38:56 +0700

    make deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import StateLessComponent from "./demo-components/StateLessComponent";
+import StateFullComponent from "./demo-components/StateFullComponent";
+import ExampleDivComponent from "./example-div-component/index";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* goi component */}
+      {/* <StateLessComponent />
+      <StateFullComponent /> */}
+      <ExampleDivComponent />
     </div>
   );
 }
---
+++ src/demo-components/StateLessComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function StateLessComponent() {
+  return (
+    <div>
+      <h1>stateLessComponent</h1>
+    </div>
+  );
+}
+export default StateLessComponent;
